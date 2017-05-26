982d0d371cad7ead258580a719a9c80425b650aa
Mayank Awasthi <mayankawasthi07@gmail.com>
2017-05-26 17:31:07 +0530

    update for package file

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nodexpert:react-custom-form',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Package to create react form by using json data',
   // URL to the Git repository containing the source code for this package.
@@ -9,10 +9,6 @@ Package.describe({
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
-
-Npm.depends({
-  "react": '15.3.0'
-})
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4.2.3');
